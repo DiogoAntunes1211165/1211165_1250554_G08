1f54e22deeced94dd842b6bf16ec8b53637381cf
DiogoAntunes <1211165@isep.ipp.pt>
2025-10-26 16:58:12 +0000

    MongoDB persistence implementation refactoring - Fix

--- src/main/java/pt/psoft/g1/psoftg1/shared/repositories/nonrelational/PhotoRepositoryMongoDBImpl.java
+++ src/main/java/pt/psoft/g1/psoftg1/shared/repositories/nonrelational/PhotoRepositoryMongoDBImpl.java
@@ -1,5 +1,7 @@
 package pt.psoft.g1.psoftg1.shared.repositories.nonrelational;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import pt.psoft.g1.psoftg1.shared.repositories.PhotoRepository;
@@ -10,6 +12,8 @@ public class PhotoRepositoryMongoDBImpl implements PhotoRepository {
 
     private final PhotoRepositoryMongoDB photoRepositoryMongoDB;
 
+    @Autowired
+    @Lazy
     public PhotoRepositoryMongoDBImpl(PhotoRepositoryMongoDB photoRepositoryMongoDB) {
         this.photoRepositoryMongoDB = photoRepositoryMongoDB;
     }
