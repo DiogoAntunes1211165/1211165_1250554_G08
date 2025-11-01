8968fc75b221ce0944023d32cbe8ad81662f449f
tiagomiguel55 <1250554@isep.ipp.pt>
2025-11-01 23:44:36 +0000

    Refactor test package structure for Author, Lending, and Repository tests

--- src/test/java/pt/psoft/g1/psoftg1/integrationTests/authormanagement/api/AuthorControllerOpaqueTest.java
+++ src/test/java/pt/psoft/g1/psoftg1/integrationTests/authormanagement/api/AuthorControllerOpaqueTest.java
@@ -1,4 +1,4 @@
-package pt.psoft.g1.psoftg1.integrationTests;
+package pt.psoft.g1.psoftg1.integrationTests.authormanagement.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -6,7 +6,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
